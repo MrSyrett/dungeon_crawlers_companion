05f9da6db14daba6d532d3f845159533587b038e
Michael Syrett <MrSyrett@gmail.com>
2026-09-18 14:57:26 -0700

    GB

--- public/tools-data/gb-sheet-builder.js
+++ public/tools-data/gb-sheet-builder.js
@@ -134,7 +134,7 @@
       notes: '', tagPhysical: '', tagPersonality: '',
       traits: { Brains: traits.Brains, Muscles: traits.Muscles, Moves: traits.Moves, Cool: traits.Cool },
       talents: { Brains: { name: talent.Brains }, Muscles: { name: talent.Muscles }, Moves: { name: talent.Moves }, Cool: { name: talent.Cool } },
-      residence: '', phone: '', telex: '', hospital: 0,
+      residence: '', phone: '', telex: '',
       bp: 20, gear: gear.slice()
     };
     var prev = (typeof collectSheet === 'function') ? collectSheet() : {};
